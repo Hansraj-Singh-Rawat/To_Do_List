e3a4ddea944f87139166a7791faeef033285b0c3
Hansraj Singh <146731461+Hansraj-Singh-Rawat@users.noreply.github.com>
2023-11-13 11:38:53 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<footer class="footer" style="background-color: gray;">
+    <div class="container" style="color: white;">
+        <center>
+        <marquee behavior="disappearing" direction="left"><p>Copyright &copy; To-Do List. All Rights Reserved | Contact Us: +91 33049000002     </p></marquee>
+        </center>
+    </div>
+</footer>
